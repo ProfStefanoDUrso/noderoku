61cabb215e3e7aab075d9461643085ab93fb59a2
ProfStefanoDUrso <prof.stefano.durso@gmail.com>
2020-02-04 16:10:49 +0100

    assessment

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-// TEST
-
 const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession=require('cookie-session');
+const passport=require('passport');
+const keys=require('./config/keys');
 
+require('./models/User'); // beware: this has to be before passport otherwise there's the schema error
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
 app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000, // 30 days
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
 
 app.listen(process.env.PORT || 3000,() => {
     console.log('listening');
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
+app.get('/googlea0f1138ad69fe3bf.html',(req,res) => {
+    res.sendFile(path.join(__dirname,'./googlea0f1138ad69fe3bf.html'));
+});
+    
 app.get('*',(req,res) => {
     res.sendFile(path.join(__dirname,'./index.html'));
 });
