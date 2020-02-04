4bc4e2f6906bcd97d8a7523b9ae21992b115d85f
ProfStefanoDUrso <prof.stefano.durso@gmail.com>
2020-02-04 17:08:41 +0100

    change dev-prod

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,5 @@
-module.exports = {
-    googleClientID:'1016378166956-4h5h28oirvgk95upjeu3bqdv3jpri9kr.apps.googleusercontent.com',
-    googleClientSecret:'Anj1_1mLZ9qqcgYZte8m3PdK',
-    mongoURI:'mongodb+srv://myMongoDBUser:myMongoDBPassword@cluster0-0evaf.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'sdfsffuhewufhweifhksdbf'
-
-    // MongoDB
-    // user: myMongoDBUser / myMongoDBProdUser
-    // pwd: myMongoDBPassword / myMongoDBProdPassword
-
-    ,mongoProdURI: 'mongodb+srv://myMongoDBProdUser:myMongoDBProdPassword@cluster0-ijbd5.mongodb.net/test?retryWrites=true&w=majority'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports=require('./prod');
+} else {
+    module.exports=require('./dev');
+}
