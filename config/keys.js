d287ffba8baaf26109f65ceb9dc7407ab6a8d375
ProfStefanoDUrso <prof.stefano.durso@gmail.com>
2020-02-04 16:14:32 +0100

    config

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID:'1016378166956-4h5h28oirvgk95upjeu3bqdv3jpri9kr.apps.googleusercontent.com',
+    googleClientSecret:'Anj1_1mLZ9qqcgYZte8m3PdK',
+    mongoURI:'mongodb+srv://myMongoDBUser:myMongoDBPassword@cluster0-0evaf.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'sdfsffuhewufhweifhksdbf'
+
+    // MongoDB
+    // user: myMongoDBUser / myMongoDBProdUser
+    // pwd: myMongoDBPassword / myMongoDBProdPassword
+
+    ,mongoProdURI: 'mongodb+srv://myMongoDBProdUser:myMongoDBProdPassword@cluster0-ijbd5.mongodb.net/test?retryWrites=true&w=majority'
+};
